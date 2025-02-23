f5cc3d4d5fb5c532b053c2b436c2a4c3364bb265
santiago penilla <santiagopenillaramirez@gmail.com>
2025-02-23 15:21:23 -0500

    arbol .pl corregido

--- trabajo-1/arbol.pl
+++ trabajo-1/arbol.pl
@@ -1,4 +1,3 @@
-
 mujer(marge).
 mujer(patty).
 mujer(selma).
@@ -7,32 +6,51 @@ mujer(jacqueline).
 mujer(ling).
 mujer(mona).
 mujer(maggie).
-hombre(homer).
+
+hombre(homero).
 hombre(herbert).
 hombre(abraham).
 hombre(clancy).
 hombre(bart).
-padre_de(abraham,homero).
-padre_de(abraham,herbert).
-padre_de(clancy,marge).
-padre_de(clancy,patty).
-padre_de(clancy,selma).
-padre_de(homero,bart).
-padre_de(homero,lisa).
-padre_de(homero,maggie).
-madre_de(mona,homero).
-madre_de(mona,herbert).
-madre_de(jacqueline,marge).
-madre_de(jacqueline,selma).
-madre_de(jacqueline,patty).
-madre_de(marge,lisa).
-madre_de(marge,bart).
-madre_de(marge,maggie).
-madre_de(selma,ling).
 
-abuelo_de(X,Y):-(hombre(X),padre_de(Z,Y)),padre_de(X,Z).
-abuela_de(X,Y):-(mujer(X),madre_de(X,Z)),madre_de(Z,Y).
-hermana_de(X,Y):-(mujer(X),madre_de(Z,X),madre_de(Z,Y)),(padre_de(F,X),padre_de(F,Y)).
-hermano_de(X,Y):-(hombre(X),madre_de(Z,X),madre_de(Z,Y)),(padre_de(F,X),padre_de(F,Y)).
-tio_de(X,Y):-(hombre(X),hermano_de(X,Z)),padre_de(Z,Y).
-tia_de(X,Y):-(mujer(X),hermana_de(X,Z)),madre_de(Z,Y).
+padre_de(abraham, homero).
+padre_de(abraham, herbert).
+padre_de(clancy, marge).
+padre_de(clancy, patty).
+padre_de(clancy, selma).
+padre_de(homero, bart).
+padre_de(homero, lisa).
+padre_de(homero, maggie).
+madre_de(mona, homero).
+madre_de(mona, herbert).
+madre_de(jacqueline, marge).
+madre_de(jacqueline, selma).
+madre_de(jacqueline, patty).
+madre_de(marge, lisa).
+madre_de(marge, bart).
+madre_de(marge, maggie).
+madre_de(selma, ling).
+
+abuelo_de(X, Y) :- 
+    hombre(X), 
+    (padre_de(X, Z) ; madre_de(X, Z)), 
+    (padre_de(Z, Y) ; madre_de(Z, Y)).
+
+abuela_de(X, Y) :- 
+    mujer(X), 
+    madre_de(X, Z), 
+    (padre_de(Z, Y) ; madre_de(Z, Y)).
+
+hermana_de(X, Y) :- 
+    mujer(X), 
+    ((padre_de(P, X), padre_de(P, Y)) ; 
+    (madre_de(M, X), madre_de(M, Y))).
+
+hermano_de(X, Y) :- 
+    hombre(X),  
+    ((padre_de(P, X), padre_de(P, Y)) ; 
+    (madre_de(M, X), madre_de(M, Y))).
+
+tio_de(X, Y) :- hombre(X), hermano_de(X, Z), (padre_de(Z, Y) ; madre_de(Z, Y)).
+tia_de(X, Y) :- mujer(X), hermana_de(X, Z), (padre_de(Z, Y) ; madre_de(Z, Y)).
+
