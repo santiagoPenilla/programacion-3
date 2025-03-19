16a8c2052d938854d105b46cee7da2a6a705fcef
WMike <miguelwcaamaya0911@gmail.com>
2025-03-18 20:07:03 -0500

    Añadidas reglas de parientes

--- trabajo-4/datos_familiar.pl
+++ trabajo-4/datos_familiar.pl
@@ -5,4 +5,50 @@ progenitor(jose, ana).
 progenitor(jose, patricia).
 progenitor(patricia, jaime).
 
-% Reglas
+hombre(jose).
+hombre(tomas).
+hombre(jaime).
+
+mujer(clara).
+mujer(isabel).
+mujer(ana).
+mujer(patricia).
+
+dif(X, Y) :- X \= Y.
+
+es_madre(X) :-
+    mujer(X),
+    progenitor(X, _).
+
+es_padre(X) :-
+    hombre(X),
+    progenitor(X, _).
+
+es_hijo(X) :-
+    progenitor(_, X).
+
+hermana_de(X, Y) :-
+    mujer(X),
+    dif(X, Y),
+    progenitor(Z, X),
+    progenitor(Z, Y).
+
+abuelo_de(X, Y) :-
+    hombre(X),
+    progenitor(X, Z),
+    progenitor(Z, Y).
+
+abuela_de(X, Y) :-
+    mujer(X),
+    progenitor(X, Z),
+    progenitor(Z, Y).
+
+hermanos(X, Y) :-
+    dif(X, Y),
+    progenitor(Z, X),
+    progenitor(Z, Y).
+
+tia(X, Y) :-
+    mujer(X),
+    progenitor(Z, Y),
+    hermana_de(X, Z).
