31dc8fb466f57abd926f0fbefa6c7a36de263f84
WMike <miguelwcaamaya0911@gmail.com>
2025-04-02 14:23:21 -0500

    Hechas primeras reglas y estructuras básicas

---
+++ trabajo-6/familia-simpson.pl
@@ -0,0 +1,34 @@
+mujer([marge, patty, selma, lisa, jacqueline, ling, mona, maggie]).
+hombre([herbert, abraham, clancy, bart, homero]).
+
+es_mujer(X) :- mujer(L), member(X, L).
+es_hombre(X) :- hombre(L), member(X, L).
+
+padre_de(abraham, [homero, herbert]).
+padre_de(clancy, [marge, patty, selma]).
+padre_de(homero, [bart, lisa, maggie]).
+
+madre_de(mona, [homero, herbert]).
+madre_de(jacqueline, [marge, selma, patty]).
+madre_de(marge, [lisa, bart, maggie]).
+madre_de(selma, [ling]).  % Se coloca ling entre corchetes para mantener la consistencia
+
+abuelo_de(X, Y) :- 
+    es_hombre(X), 
+    (padre_de(X, Hijos) ; madre_de(X, Hijos)), 
+    member(Z, Hijos),
+    (padre_de(Z, Nietos) ; madre_de(Z, Nietos)),
+    member(Y, Nietos).
+
+abuela_de(X, Y) :- 
+    es_mujer(X), 
+    madre_de(X, Hijos), 
+    member(Z, Hijos),
+    (padre_de(Z, Nietos) ; madre_de(Z, Nietos)),
+    member(Y, Nietos).
+
+hermana_de(X, Y) :- 
+    es_mujer(X), 
+    ((padre_de(P, Hijos), member(X, Hijos), member(Y, Hijos));
+     (madre_de(M, Hijos), member(X, Hijos), member(Y, Hijos))),
+    X \= Y.
