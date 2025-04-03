f9e3af92171784bca289c24b405d7756ba23de9c
WMike <miguelwcaamaya0911@gmail.com>
2025-04-02 19:21:45 -0500

    Añadidos cambios con estructuras

--- trabajo-6/familia-simpson.pl
+++ trabajo-6/familia-simpson.pl
@@ -1,14 +1,56 @@
-mujer([marge, patty, selma, lisa, jacqueline, ling, mona, maggie]).
-hombre([herbert, abraham, clancy, bart, homero]).
+es_mujer(X):- 
+    member(X, [mujer(jacqueline), mujer(ling), mujer(lisa), mujer(maggie), mujer(marge), mujer(mona), mujer(patty), mujer(selma)]).
 
-es_mujer(X) :- mujer(L), member(X, L).
-es_hombre(X) :- hombre(L), member(X, L).
+es_hombre(X):- 
+    member(X, [hombre(abraham), hombre(bart), hombre(clancy), hombre(herbert), hombre(homero)]).
 
-padre_de(abraham, [homero, herbert]).
-padre_de(clancy, [marge, patty, selma]).
-padre_de(homero, [bart, lisa, maggie]).
+    (hombre(clancy), mujer(marge)), (hombre(clancy), mujer(patty)), (hombre(clancy), mujer(selma))]).
 
-madre_de(mona, [homero, herbert]).
-madre_de(jacqueline, [marge, selma, patty]).
-madre_de(marge, [lisa, bart, maggie]).
-madre_de(selma, [ling]).
+madre_de(X,Y):- 
+    member((X,Y),
+    [(mujer(marge), hombre(bart)), (mujer(marge), mujer(lisa)), (mujer(marge), mujer(maggie)), 
+     (mujer(mona), hombre(herbert)), (mujer(mona), hombre(homero)), 
+     (mujer(jacqueline), mujer(marge)), (mujer(jacqueline), mujer(patty)), (mujer(jacqueline), mujer(selma)), 
+     (mujer(selma), mujer(ling))]).
+
+padre_de(X,Y):- 
+    member((X,Y),
+    [(hombre(homero), hombre(bart)), (hombre(homero), mujer(lisa)), (hombre(homero), mujer(maggie)), 
+     (hombre(abraham), hombre(herbert)), (hombre(abraham), hombre(homero)), 
+
+
+abuela_de(X, Z) :- madre_de(X, Y), (padre_de(Y, Z) ; madre_de(Y, Z)).
+abuelo_de(X, Z) :- padre_de(X, Y), (padre_de(Y, Z) ; madre_de(Y, Z)).
+
+dif(A, B):- A \== B.
+
+hermano_de(X, Y) :- 
+    es_hombre(X), dif(X,Y),
+    (padre_de(Z, X), padre_de(Z, Y));
+    (madre_de(Z, X), madre_de(Z, Y)), es_hombre(X), dif(X,Y).
+
+hermana_de(X, Y) :- 
+    es_mujer(X), dif(X,Y),
+    (padre_de(Z, X), padre_de(Z, Y));
+    (madre_de(Z, X), madre_de(Z, Y)), es_mujer(X), dif(X,Y).
+
+tia_de(X, Z) :-  
+    es_mujer(X), 
+    hermana_de(X, Y), 
+    (padre_de(Y, Z) ; madre_de(Y, Z)).
+
+tio_de(X, Z) :-  
+    es_hombre(X), 
+    hermano_de(X, Y), 
+    (padre_de(Y, Z) ; madre_de(Y, Z)).
+
+
+prima_de(X, Z) :- 
+    (tio_de(Y, Z) ; tia_de(Y, Z)),  
+    (madre_de(Y, X) ; padre_de(Y, X)), 
+    es_mujer(X).
+
+primo_de(X, Z) :- 
+    (tio_de(Y, Z) ; tia_de(Y, Z)),  
+    (madre_de(Y, X) ; padre_de(Y, X)), 
+    es_hombre(X).
